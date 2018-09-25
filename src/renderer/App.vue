cea9adb4fd74f469ffd3a550887511296fa07a08
LeoMartinDev <leo1.martin@epitech.eu>
2018-09-25 13:13:26 +0200

    base

---
+++ src/renderer/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-card>
+        <v-tabs v-model="active" color="cyan" dark slider-color="yellow" show-arrows app>
+          <v-tab v-for="tab in tabs" :key="tab.id" ripple>
+            {{ tab.title }}
+          </v-tab>
+          <v-tabs-items>
+            <v-tab-item v-for="(tab, i) of tabs" :id="tab.id" :key="tab.id">
+              <webview :src="tab.src"></webview>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-tabs>
+        <v-btn absolute dark fab bottom right icon color="pink" @click="newTab">
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-card>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import { v4 } from 'uuid';
+
+export default {
+  name: "pbrowser",
+  data: () => ({
+    active: null,
+    tabs: []
+  }),
+  methods: {
+    newTab() {
+      this.tabs.push({
+        id: v4(),
+        src: 'https://google.com',
+      });
+    }
+  }
+};
+</script>
+
+<style>
+/* CSS */
+</style>
